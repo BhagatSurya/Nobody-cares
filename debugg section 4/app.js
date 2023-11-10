5a58ccc2d419414a1025e01fd4cd310fed9d9438
inatom.surya <129405768+Eduin2024@users.noreply.github.com>
2023-11-10 21:03:39 +0530

    day 11

--- debugg section 4/app.js
+++ debugg section 4/app.js
@@ -2,9 +2,11 @@ const http = require('http')
 const routes =  require('./routes')
 // you can save npm pakages as --save -dev it will add to production 
 // you use  --save as production pakage asn y
-// yyou can ude --g for global 
-const server = http.createServer(routes)
+// yyou can ude --g for global
 
+//type of error 
+//1) syntax error 2) runtime 3) Logic errors 
+const server = http.createServer(routes)
 
 server.listen(3000, () => {
     console.log('Server is listening on port 3000');
