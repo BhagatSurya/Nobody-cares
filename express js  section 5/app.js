53dcd97569a3c7155a0560faa48b20f12c0c7731
inatom.surya <129405768+Eduin2024@users.noreply.github.com>
2023-11-12 18:33:31 +0530

    day 12

--- express js  section 5/app.js
+++ express js  section 5/app.js
@@ -1,20 +1,23 @@
 const http = require('http')
-
+const bodyparser = require('body-parser')
 const express = require('express')
 
 const app = express()
 //middleware
+ app.use(bodyparser.urlencoded())
 
-app.use((req,res,next) =>{
-    console.log('In the middleware')
-    next()
+app.get('/add-prodct',(req,res,next) =>{
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="sumit">ADD PRODUCT</button></form>')})
+
+app.post("/product",(req,res,next) => {
+    console.log(req.body)
+    res.redirect('/')
 })
 
-app.use((req,res,next) =>{
-    console.log('another In the middleware')
+app.get('/',(req,res,next) =>{
     res.send('<h1>hello Bhagat surya</h1>')
+    
 })
-
 
 app.listen(3000)
 
